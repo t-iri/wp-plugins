9247aa1b5f2b35c9408c04c36441129b1488b65a
t-iri <iri@ng-life.jp>
2016-03-26 19:03:08 +0900

    change
    change

--- ck-multiuse-functions/ck-multiuse-funcions.php
+++ ck-multiuse-functions/ck-multiuse-funcions.php
@@ -1,25 +1,11 @@
 <?php
 /*
 Plugin Name: CK Multiuse Functions
-Plugin URI:  http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: Load useful functions
 Version:     1.1
 Author:      iri
 Author URI:  http://cunelwork.co.jp/
 License:     GPL3
- 
-{Plugin Name} is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-any later version.
- 
-{Plugin Name} is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
- 
-You should have received a copy of the GNU General Public License
-along with {Plugin Name}. If not, see {License URI}.
 */
 if(defined('CKMF_VERSION')) return;
 define('CKMF_VERSION', '1.1');
